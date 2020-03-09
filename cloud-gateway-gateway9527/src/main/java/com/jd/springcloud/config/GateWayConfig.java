3845aeb8f0b7e8ef422703cfdc97ceb610b1a5e2
coderTomato <570547651@qq.com>
2020-03-09 23:43:53 +0800

    提交Gateway代码

---
+++ cloud-gateway-gateway9527/src/main/java/com/jd/springcloud/config/GateWayConfig.java
@@ -0,0 +1,27 @@
+package com.jd.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lijun
+ * @create 2020-03-09 20:45
+ */
+@Configuration
+public class GateWayConfig {
+
+    /**
+     * 配置了一个route-name的路由规则
+     * 当访问地址http://localhost:9527/guonei时会自动转发到地址 http://news.baidu.com/guonei
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_routh_jd",r->r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author lijun
+ * @create 2020-03-09 22:09
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
